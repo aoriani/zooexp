b5e2a814dd503e26c23c7f824a555a79e8bcc67d
André Oriani <ra078686@students.ic.unicamp.br>
2010-06-07 22:58:48 -0300

    Improvements on shell scripts so they are independent from current working dir

--- scripts/start_basicserver.sh
+++ scripts/start_basicserver.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+pushd $(dirname "$0")
+
 # Starts the Basic Server that can accept multiple clients
 java -cp ../bin:../conf:../libs/zookeeper-3.3.1/lib/log4j-1.2.15.jar -Dlog4j.configuration=file:../conf/basicserver_log4j.properties br.unicamp.ic.zooexp.server.basic.BasicServer
+
+popd
--- scripts/zookeeper.sh
+++ scripts/zookeeper.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+pushd $(dirname "$0")
+
 # redirect commands to zookeeper  
 ../libs/zookeeper-3.3.1/bin/zkServer.sh "$@"
+
+popd
